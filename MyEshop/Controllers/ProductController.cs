ce33d8e611a7fd34b0db3de6dc24258de5f432f9
Mahmood Ghanbari <contact.ghanbary@gmail.com>
2020-07-04 01:48:35 -0700

    working on Product Groups

---
+++ MyEshop/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer;
+
+namespace MyEshop.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        MyEshop_DBEntities db = new MyEshop_DBEntities();
+        public ActionResult ShowGroups()
+        {
+            return PartialView(db.Product_Groups.ToList());
+        }
+    }
+}
