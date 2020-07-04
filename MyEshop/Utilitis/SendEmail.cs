46eb5606eeb7e53d0760d9e385dc75dd4ee85871
Mahmood Ghanbari <contact.ghanbary@gmail.com>
2020-07-04 00:39:41 -0700

    Initial commit

---
+++ MyEshop/Utilitis/SendEmail.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace MyEshop
+{
+    public class SendEmail
+    {
+        public static void Send(string To,string Subject,string Body)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+            mail.From = new MailAddress("cod.mahmood@gmail.com","فروشگاه مای کامپیوتر");
+            mail.To.Add(To);
+            mail.Subject = Subject;
+            mail.Body = Body;
+            mail.IsBodyHtml = true;
+
+            //System.Net.Mail.Attachment attachment;
+            // attachment = new System.Net.Mail.Attachment("c:/textfile.txt");
+            // mail.Attachments.Add(attachment);
+
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("contact.ghanbary@gmail.com", "1214255190");
+
+            SmtpServer.EnableSsl = true;
+
+            SmtpServer.Send(mail);
+
+        }
+    }
+}
